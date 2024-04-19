57f9e44f5d565388e815af47f8e5fd27c75e731a
Sergey Fedorov <vital.had@gmail.com>
2024-04-20 03:00:31 +0800

    miniaudio.h: add a missing <AvailabilityMacros.h> include

--- miniaudio.h
+++ miniaudio.h
@@ -3739,7 +3739,9 @@ extern "C" {
     #endif
 #endif
 
-
+#ifdef __APPLE__
+    #include <AvailabilityMacros.h>
+#endif
 
 #if defined(__LP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__)) || defined(_M_X64) || defined(__ia64) || defined(_M_IA64) || defined(__aarch64__) || defined(_M_ARM64) || defined(__powerpc64__) || defined(__ppc64__)
     #define MA_SIZEOF_PTR   8
